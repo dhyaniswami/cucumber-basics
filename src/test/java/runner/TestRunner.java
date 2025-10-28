646a10562acac5a7dc3a79171426bd6a4259aee9
dhyaniswami <sakariyadhaval@hotmail.com>
2025-10-28 16:07:26 -0400

    added extent.properties file and added publish = true in cucumber options

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -6,12 +6,12 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = {"src/test/resources/features/Registration.feature"},
+@CucumberOptions(features = {"src/test/resources/features/"},
                  glue = {"stepdefinition", "hooks"},
                  //dryRun = true,
-                tags ="@Map",
+                //tags ="@Map",
              //monochrome = true,  this option is for formated result which is not used anymore due to formatted result. but we use pretty plugin for pretty result
-                 plugin = {"pretty"})
+                 plugin = {"pretty"}, publish = true)
 public class TestRunner {
 
 
